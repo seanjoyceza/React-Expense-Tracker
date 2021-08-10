27af7f67a3ab0be24f69f0bdd10496f108e21200
Sean Joyce <joycesean702@gmail.com>
2021-08-10 08:39:27 +0100

    commit

---
+++ React Expense Tracker/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+    return (
+        <li>
+            <Card className="expense-item">
+                {/* selfclosing element if it has no attributes in between: */}
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">€{props.amount}</div>
+                </div>
+            </Card>
+        </li>
+    );
+};
+
+export default ExpenseItem;
